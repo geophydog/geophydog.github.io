0065f058c829c8a993cad5274a85cd68b4c0df09
geophydog <geophydogvon@gmail.com>
2020-09-18 15:17:49 +0800

    add new contents

---
+++ post/plot_issues.py
@@ -0,0 +1,14 @@
+---
+title: "Some Useful Settings of Python Plotting"
+date: 2020-09-18T14:52:10+08:00
+tag: ["python"]
+categories: ["virsualization"]
+---
+
+## __Pseudo-color map with polar projection__
+We uasually need to plot pseudo-color maps in polar projection besides
+except of thoese in Cartesian projection.
+Let's give an example of python code.
+```python
+
+```
